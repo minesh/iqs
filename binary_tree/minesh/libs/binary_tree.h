dee36279d44796bb01018bcfe09a469faeea5bfe
Minesh Patel <minesh@gmail.com>
2014-08-13 23:25:18 -0700

    Initial binary tree

---
+++ binary_tree/minesh/libs/binary_tree.h
@@ -0,0 +1,11 @@
+#ifndef binary_tree_h
+#define binary_tree_h
+
+typedef struct Node {
+   struct Node *left;
+   struct Node *right;
+   int data;
+} Node;
+
+
+#endif binary_tree_h
---
+++ binary_tree/minesh/src/main.c
@@ -0,0 +1,6 @@
+#include "binary_tree.h"
+
+int main (void) {
+   Node node;
+   return 0;
+}
